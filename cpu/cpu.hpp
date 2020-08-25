188b74e6767c830060419fc644dfcb7cfef70ccd
Tahar <tntmeijs@gmail.com>
2020-08-25 22:25:00 +0200

    Empty CPU class

---
+++ cpu/cpu.hpp
@@ -0,0 +1,17 @@
+#ifndef NES_CPU_HPP
+#define NES_CPU_HPP
+
+namespace nes
+{
+    /**
+     * Emulates a MOS Technology 6502 microprocessor
+     */
+    class CPU
+    {
+    public:
+
+    private:
+    };
+}
+
+#endif //! NES_CPU_HPP
