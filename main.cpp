e58cb5f853037d3a8947e11eb3b6b8113d0ddc4e
Tahar <tntmeijs@gmail.com>
2020-08-25 17:48:08 +0200

    ImGui test code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <imgui.h>
+#include <imgui-SFML.h>
+
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
@@ -6,7 +9,11 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "NES");
 	window.setVerticalSyncEnabled(true);
 
+	// Initialize ImGui for SFML
+	ImGui::SFML::Init(window);
+
 	sf::Color clearColor = sf::Color::Black;
+	float color[3] = { clearColor.r, clearColor.g, clearColor.b };
 
 	sf::Clock mainLoopClock;
 	
@@ -16,6 +23,8 @@ int main()
 
 		while (window.pollEvent(event))
 		{
+			ImGui::SFML::ProcessEvent(event);
+
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
@@ -25,7 +34,24 @@ int main()
 		// Frame delta-time
 		sf::Time delta = mainLoopClock.restart();
 
+		// Update ImGui
+		ImGui::SFML::Update(window, delta);
+
+		// Test ImGui
+		ImGui::Begin("Test Window");
+		if (ImGui::ColorEdit3("Clear Color", color))
+		{
+			clearColor.r = color[0] * 255.0f;
+			clearColor.g = color[1] * 255.0f;
+			clearColor.b = color[2] * 255.0f;
+		}
+		ImGui::End();
+
 		window.clear(clearColor);
+
+		// Render ImGui
+		ImGui::SFML::Render(window);
+
 		window.display();
 	}
 }
