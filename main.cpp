f9c476f430c7370555a94dc2baa0459ba7b7b12f
Tahar <tntmeijs@gmail.com>
2020-08-25 17:27:21 +0200

    SFML configuration [ADD] SFML CMAKE configuration. [ADD] SFML window creation code.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 
 int main()
 {
-    std::cout << "Hello, NES!" << std::endl;
-    return 0;
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "NES");
+	window.setVerticalSyncEnabled(true);
+
+	sf::Color clearColor = sf::Color::Black;
+
+	sf::Clock mainLoopClock;
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		// Frame delta-time
+		sf::Time delta = mainLoopClock.restart();
+
+		window.clear(clearColor);
+		window.display();
+	}
 }
