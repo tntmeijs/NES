82ec8dffc877b7011f9211c824851d4eb7725da4
Tahar <tntmeijs@gmail.com>
2020-08-28 14:40:17 +0200

    RAM class [ADD] Class that will act as the CPU's RAM.

---
+++ ram/ram.cpp
@@ -0,0 +1,35 @@
+#include "ram.hpp"
+
+nes::RAM::RAM() :
+	SIZE(2048),
+	Memory(nullptr)
+{}
+
+void nes::RAM::Initialize()
+{
+	Memory = new std::uint8_t[SIZE];
+}
+
+void nes::RAM::Delete()
+{
+	if (Memory)
+	{
+		delete[] Memory;
+		Memory = nullptr;
+	}
+}
+
+const std::uint8_t nes::RAM::ReadByte(std::uint16_t address) const
+{
+	return Memory[address];
+}
+
+void nes::RAM::WriteByte(std::uint16_t address, std::uint8_t value)
+{
+	Memory[address] = value;
+}
+
+const std::uint8_t* const nes::RAM::GetMemory() const
+{
+	return Memory;
+}
---
+++ ram/ram.hpp
@@ -0,0 +1,56 @@
+#ifndef NES_RAM_HPP
+#define NES_RAM_HPP
+
+#include <cstdint>
+
+namespace nes
+{
+	class RAM
+	{
+	public:
+		/**
+		 * Create a new RAM object
+		 */
+		RAM();
+
+		/**
+		 * Initialize the RAM
+		 * Allocates 2048 bytes of memory for the emulator to use
+		 */
+		void Initialize();
+
+		/**
+		 * Deallocates the block of memory
+		 */
+		void Delete();
+
+		/**
+		 * Read a byte from memory
+		 * @param	address		Address pointing to the byte to read
+		 * @return	Value of the byte at the specified memory address
+		 */
+		const std::uint8_t ReadByte(std::uint16_t address) const;
+
+		/**
+		 * Set the value of a byte in memory
+		 * @param	address		Address pointing to the byte to read
+		 * @param	value		Value to set the byte to
+		 */
+		void WriteByte(std::uint16_t address, std::uint8_t value);
+
+		/**
+		 * Retrieve a pointer to the block of memory that backs the RAM object
+		 * @return	Pointer to the raw memory block
+		 */
+		const std::uint8_t* const GetMemory() const;
+
+	public:
+		/** Size of the memory block in bytes */
+		const std::uint32_t SIZE;
+
+	private:
+		std::uint8_t* Memory;
+	};
+}
+
+#endif //! NES_RAM_HPP
