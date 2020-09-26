602ec9fd9f9952a1c0420c847db351d087db88b1
Tahar <tntmeijs@gmail.com>
2020-09-26 19:06:11 +0200

    BVC implementation

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -1248,13 +1248,36 @@ void nes::CPU::BRK(AddressingMode mode)
 
 void nes::CPU::BVC(AddressingMode mode)
 {
+	if (mode != AddressingMode::Relative)
+	{
+		// Addressing mode is not used, but it is good to check for any
+		// inconsistencies regardless
+		std::cerr << "BVC - Unknown addressing mode.\n";
+	}
+
+	CurrentCycle += 2;
+
 	// Overflow flag is clear
 	if ((P & (1 << 6)) == 0)
 	{
-		// Perform branching
+		std::uint16_t initialPC = PC;
 		std::int8_t displacement = RamRef.ReadByte(PC + 1);
+
+		// Perform branching
 		PC += displacement;
+
+		if (DidProgramCounterCrossPageBoundary(initialPC, PC))
+		{
+			CurrentCycle += 2;
+		}
+		else
+		{
+			CurrentCycle += 1;
+		}
 	}
+
+	// Always move past the branch bytes, regardless of the branch outcome
+	PC += 2;
 }
 
 void nes::CPU::BVS(AddressingMode mode)
