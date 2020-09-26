5e4d2ea8410d8d9a58006fe700207f5fa3c309f6
Tahar <tntmeijs@gmail.com>
2020-09-26 19:12:51 +0200

    LDX fixed negative and zero flags

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -1724,16 +1724,26 @@ void nes::CPU::LDX(AddressingMode mode)
 		std::cerr << "LDX - Unknown addressing mode.\n";
 	}
 
-	// Set zero flag
 	if (value == 0)
 	{
+		// Set zero flag
 		P |= (1 << 1);
 	}
+	else
+	{
+		// Unset zero flag
+		P &= ~(1 << 1);
+	}
 
-	// Set negative flag
 	if ((value & (1 << 7)) != 0)
 	{
+		// Set negative flag
 		P |= (1 << 7);
+	}
+	else
+	{
+		// Unset negative flag
+		P &= ~(1 << 7);
 	}
 
 	// Store in the X register
