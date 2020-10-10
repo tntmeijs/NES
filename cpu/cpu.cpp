e6edb4820732ed8a040bc1fa2cb0fb97be5b1b66
Tahar <tntmeijs@gmail.com>
2020-10-10 15:59:20 +0200

    CMP implementation

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -1632,6 +1632,87 @@ void nes::CPU::CLV(AddressingMode mode)
 
 void nes::CPU::CMP(AddressingMode mode)
 {
+	std::uint8_t value = RamRef.ReadByte(GetTargetAddress(mode));
+	std::uint8_t result = A - value;
+
+	if (A >= value)
+	{
+		SetStatusFlag(StatusFlags::Carry);
+	}
+	else
+	{
+		ClearStatusFlag(StatusFlags::Carry);
+	}
+
+	if (A == value)
+	{
+		SetStatusFlag(StatusFlags::Zero);
+	}
+	else
+	{
+		ClearStatusFlag(StatusFlags::Zero);
+	}
+
+	if (IsNthBitSet(result, 7))
+	{
+		SetStatusFlag(StatusFlags::Negative);
+	}
+	else
+	{
+		ClearStatusFlag(StatusFlags::Negative);
+	}
+
+	if (mode == AddressingMode::Immediate)
+	{
+		PC += 2;
+		CurrentCycle += 2;
+	}
+	else if (mode == AddressingMode::ZeroPage)
+	{
+		PC += 2;
+		CurrentCycle += 3;
+	}
+	else if (mode == AddressingMode::ZeroPageX)
+	{
+		PC += 2;
+		CurrentCycle += 4;
+	}
+	else if (mode == AddressingMode::Absolute)
+	{
+		PC += 3;
+		CurrentCycle += 4;
+	}
+	else if (mode == AddressingMode::AbsoluteX || mode == AddressingMode::AbsoluteY)
+	{
+		std::uint16_t initialPC = PC;
+		PC += 3;
+		CurrentCycle += 4;
+
+		if (DidProgramCounterCrossPageBoundary(initialPC, PC))
+		{
+			++CurrentCycle;
+		}
+	}
+	else if (mode == AddressingMode::IndirectX)
+	{
+		PC += 2;
+		CurrentCycle += 6;
+	}
+	else if (mode == AddressingMode::IndirectY)
+	{
+		std::uint16_t initialPC = PC;
+		PC += 2;
+		CurrentCycle += 5;
+
+		if (DidProgramCounterCrossPageBoundary(initialPC, PC))
+		{
+			++CurrentCycle;
+		}
+	}
+	else
+	{
+		std::cerr << "CMP - Unknown addressing mode.\n";
+	}
 }
 
 void nes::CPU::CPX(AddressingMode mode)
