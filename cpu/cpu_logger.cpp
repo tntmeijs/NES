631a5cc8ddc8a66c8cfcdc6a262fa499a47b46e6
Tahar <tntmeijs@gmail.com>
2020-10-17 13:25:45 +0200

    CPU logger stream construction is now a public static function to allow other objects to call it

--- cpu/cpu_logger.cpp
+++ cpu/cpu_logger.cpp
@@ -5,7 +5,56 @@
 #include <ios>		// std::uppercase / std::hex / std::dec
 #include <iomanip>	// std::setfill / std::setw
 #include <iostream>
-#include <sstream>
+
+std::stringstream nes::CpuLogger::ConstructStreamFromData(const CPU& cpuRef, std::string_view opName, std::uint8_t opSize)
+{
+	std::uint16_t programCounter = cpuRef.GetProgramCounter();
+
+	std::stringstream stream;
+
+	// Hexadecimal mode
+	stream << std::uppercase << std::hex;
+
+	// Every 8-bit value is a 16-bit value to make the output stream treat all
+	// 8-bit values as numbers instead of characters
+	std::uint16_t A = cpuRef.GetRegister(CPU::RegisterType::A);
+	std::uint16_t X = cpuRef.GetRegister(CPU::RegisterType::X);
+	std::uint16_t Y = cpuRef.GetRegister(CPU::RegisterType::Y);
+	std::uint16_t P = cpuRef.GetRegister(CPU::RegisterType::P);
+	std::uint16_t SP = cpuRef.GetRegister(CPU::RegisterType::SP);
+
+	// Write current address of the program counter
+	stream << programCounter << "  ";
+
+	// Display instruction bytes
+	for (std::uint8_t i = 0; i < opSize; ++i)
+	{
+		stream << std::setfill('0') << std::setw(2) << static_cast<std::uint16_t>(cpuRef.ReadRamValueAtAddress(programCounter + i)) << ' ';
+	}
+
+	// An instruction may use up to three bytes
+	// Add padding to make lines align nicely
+	for (std::uint8_t i = 0; i < 3 - opSize; ++i)
+	{
+		stream << "   ";
+	}
+	stream << ' ';
+
+	// Write the instruction's Assembly name
+	stream << opName << '\t';
+
+	// Padding
+	stream << std::setfill(' ') << std::setw(30);
+
+	// Write register states
+	stream << "A:" << std::setfill('0') << std::setw(2) << A << ' ';
+	stream << "X:" << std::setfill('0') << std::setw(2) << X << ' ';
+	stream << "Y:" << std::setfill('0') << std::setw(2) << Y << ' ';
+	stream << "P:" << std::setfill('0') << std::setw(2) << P << ' ';
+	stream << "SP:" << SP << ' ';
+	stream << "PPU:  " << "     " << ' ';
+	stream << "CYC:" << std::dec << cpuRef.GetCurrentCycle();
+}
 
 nes::CpuLogger::CpuLogger(const CPU& cpuRef, const RAM& ramRef) :
 	CpuRef(cpuRef),
@@ -24,52 +73,7 @@ nes::CpuLogger::~CpuLogger()
 
 void nes::CpuLogger::LogOperation(std::string_view name, std::uint8_t num)
 {
-	std::uint16_t programCounter = CpuRef.GetProgramCounter();
-
-	std::stringstream stream;
-
-	// Hexadecimal mode
-	stream << std::uppercase << std::hex;
-
-	// Every 8-bit value is a 16-bit value to make the output stream treat all
-	// 8-bit values as numbers instead of characters
-	std::uint16_t A = CpuRef.GetRegister(CPU::RegisterType::A);
-	std::uint16_t X = CpuRef.GetRegister(CPU::RegisterType::X);
-	std::uint16_t Y = CpuRef.GetRegister(CPU::RegisterType::Y);
-	std::uint16_t P = CpuRef.GetRegister(CPU::RegisterType::P);
-	std::uint16_t SP = CpuRef.GetRegister(CPU::RegisterType::SP);
-
-	// Write current address of the program counter
-	stream << programCounter << "  ";
-
-	// Display instruction bytes
-	for (std::uint8_t i = 0; i < num; ++i)
-	{
-		stream << std::setfill('0') << std::setw(2) << static_cast<std::uint16_t>(RamRef.ReadByte(programCounter + i)) << ' ';
-	}
-
-	// An instruction may use up to three bytes
-	// Add padding to make lines align nicely
-	for (std::uint8_t i = 0; i < 3 - num; ++i)
-	{
-		stream << "   ";
-	}
-	stream << ' ';
-
-	// Write the instruction's Assembly name
-	stream << name << '\t';
-
-	// Padding
-	stream << std::setfill(' ') << std::setw(30);
-
-	// Write register states
-	stream << "A:" << std::setfill('0') << std::setw(2) << A << ' ';
-	stream << "X:" << std::setfill('0') << std::setw(2) << X << ' ';
-	stream << "Y:" << std::setfill('0') << std::setw(2) << Y << ' ';
-	stream << "P:" << std::setfill('0') << std::setw(2) << P << ' ';
-	stream << "SP:" << SP << ' ';
-	stream << "PPU:  " << "     " << ' ';
-	stream << "CYC:" << std::dec << CpuRef.GetCurrentCycle();
+	std::stringstream stream = ConstructStreamFromData(CpuRef, name, num);
 
 	// Output to the console window
 	std::cout << stream.str() << std::endl;
--- cpu/cpu_logger.hpp
+++ cpu/cpu_logger.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <fstream>
+#include <sstream>
 #include <string_view>
 
 namespace nes
@@ -15,6 +16,17 @@ namespace nes
 	 */
 	class CpuLogger
 	{
+	public:
+		/**
+		 * Utility function to help construct a string stream object that holds
+		 * information about the current CPU instruction and the state of the CPU
+		 * @param	cpuRef	Reference to the CPU
+		 * @param	opName	Name of the current operation
+		 * @param	opSize	Number of bytes used by the current operation
+		 * @return	String stream object with debug information
+		 */
+		static std::stringstream ConstructStreamFromData(const CPU& cpuRef, std::string_view opName, std::uint8_t opSize);
+
 	public:
 		/**
 		 * Create a new CPU logger object
