bb81cd973fb81048ad4f6e11cb9dd43637e89527
Tahar <tntmeijs@gmail.com>
2020-10-24 14:59:09 +0200

    ASL implementation

--- cpu/instructions/cpu_instruction_op_asl.cpp
+++ cpu/instructions/cpu_instruction_op_asl.cpp
@@ -1,6 +1,5 @@
 #include "cpu_instruction_op_asl.hpp"
-
-#include <iostream>
+#include "cpu/cpu.hpp"
 
 nes::CpuInstructionOpASL::CpuInstructionOpASL(CPU& cpuRef, AddressingMode addressingMode) :
 	CpuInstructionBase(cpuRef, addressingMode, "ASL")
@@ -8,5 +7,19 @@ nes::CpuInstructionOpASL::CpuInstructionOpASL(CPU& cpuRef, AddressingMode addres
 
 void nes::CpuInstructionOpASL::ExecuteImpl()
 {
-	// Not implemented
+	std::uint8_t old = CpuRef.A;
+	CpuRef.A = (CpuRef.A << 1);
+
+	// Set carry to the old contents of bit 7
+	if ((old & static_cast<std::uint8_t>(StatusFlags::Negative)) != 0)
+	{
+		CpuRef.SetStatusFlag(StatusFlags::Carry);
+	}
+	else
+	{
+		CpuRef.ClearStatusFlag(StatusFlags::Carry);
+	}
+
+	CpuRef.UpdateZeroStatusFlag(CpuRef.A);
+	CpuRef.UpdateNegativeStatusFlag(CpuRef.A);
 }
