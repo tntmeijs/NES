0833022658c53315ada40484be3b85e0544e76ed
Tahar <tntmeijs@gmail.com>
2020-10-24 18:25:58 +0200

    RTI implementation

--- cpu/instructions/cpu_instruction_op_rti.cpp
+++ cpu/instructions/cpu_instruction_op_rti.cpp
@@ -3,8 +3,16 @@
 
 nes::CpuInstructionOpRTI::CpuInstructionOpRTI(CPU& cpuRef, AddressingMode addressingMode) :
 	CpuInstructionBase(cpuRef, addressingMode, "RTI")
-{}
+{
+	AutoUpdateProgramCounter = false;
+}
 
 void nes::CpuInstructionOpRTI::ExecuteImpl()
 {
+	CycleCount = 6;
+
+	CpuRef.P = CpuRef.PopStack();
+	std::uint8_t msb = CpuRef.PopStack();
+	std::uint8_t lsb = CpuRef.PopStack();
+	CpuRef.PC = ((msb >> 8) | lsb);
 }
