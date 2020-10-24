14d0288bbefbad81d3e260f23d30c15981435ce2
Tahar <tntmeijs@gmail.com>
2020-10-24 16:26:28 +0200

    BRK implementation

--- cpu/instructions/cpu_instruction_op_brk.cpp
+++ cpu/instructions/cpu_instruction_op_brk.cpp
@@ -1,12 +1,28 @@
 #include "cpu_instruction_op_brk.hpp"
-
-#include <iostream>
+#include "cpu/cpu.hpp"
 
 nes::CpuInstructionOpBRK::CpuInstructionOpBRK(CPU& cpuRef, AddressingMode addressingMode) :
 	CpuInstructionBase(cpuRef, addressingMode, "BRK")
-{}
+{
+	AutoUpdateProgramCounter = false;
+}
 
 void nes::CpuInstructionOpBRK::ExecuteImpl()
 {
-	// Not implemented
+	CycleCount = 7;
+
+	// Turn program counter into two bytes so it can be pushed to the stack
+	std::uint8_t lsb = (CpuRef.PC & 0x00FF);
+	std::uint8_t msb = ((CpuRef.PC & 0xFF00) >> 8);
+
+	// Save state on the stack
+	CpuRef.PushStack(msb);
+	CpuRef.PushStack(lsb);
+	CpuRef.PushStack(CpuRef.P);
+	CpuRef.P |= (1 << 4);
+
+	// Set program counter to the IRQ interrupt vector at 0xFFFE and 0xFFFF
+	lsb = CpuRef.ReadRamValueAtAddress(0xFFFE);
+	msb = CpuRef.ReadRamValueAtAddress(0xFFFF);
+	CpuRef.PC = (((msb) >> 8) | lsb);
 }
