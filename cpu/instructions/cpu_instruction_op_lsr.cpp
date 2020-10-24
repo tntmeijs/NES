2c97b3404bd4331cfec9a0682ae3f680a620fe45
Tahar <tntmeijs@gmail.com>
2020-10-24 16:48:40 +0200

    LSR implementation

--- cpu/instructions/cpu_instruction_op_lsr.cpp
+++ cpu/instructions/cpu_instruction_op_lsr.cpp
@@ -1,10 +1,68 @@
 #include "cpu_instruction_op_lsr.hpp"
 #include "cpu/cpu.hpp"
 
+#include <iostream>
+
 nes::CpuInstructionOpLSR::CpuInstructionOpLSR(CPU& cpuRef, AddressingMode addressingMode) :
 	CpuInstructionBase(cpuRef, addressingMode, "LSR")
 {}
 
 void nes::CpuInstructionOpLSR::ExecuteImpl()
 {
+	if (InstructionAddressingMode == AddressingMode::Accumulator)
+	{
+		CycleCount = 2;
+	
+		// Need to shift in the accumulator
+		std::uint8_t old = CpuRef.A;
+		CpuRef.A = (CpuRef.A >> 1);
+
+		if ((old & static_cast<std::uint8_t>(StatusFlags::Carry)) != 0)
+		{
+			CpuRef.SetStatusFlag(StatusFlags::Carry);
+		}
+		else
+		{
+			CpuRef.ClearStatusFlag(StatusFlags::Carry);
+		}
+
+		CpuRef.UpdateZeroStatusFlag(CpuRef.A);
+		CpuRef.UpdateNegativeStatusFlag(CpuRef.A);
+	}
+	else
+	{
+		// Need to shift in a memory location
+		std::uint8_t value = CpuRef.ReadRamValueAtAddress(CpuRef.GetTargetAddress(InstructionAddressingMode));
+		std::uint8_t old = value;
+		value = (value >> 1);
+
+		if ((old & static_cast<std::uint8_t>(StatusFlags::Carry)) != 0)
+		{
+			CpuRef.SetStatusFlag(StatusFlags::Carry);
+		}
+		else
+		{
+			CpuRef.ClearStatusFlag(StatusFlags::Carry);
+		}
+
+		CpuRef.UpdateZeroStatusFlag(value);
+		CpuRef.UpdateNegativeStatusFlag(value);
+
+		if (InstructionAddressingMode == AddressingMode::ZeroPage)
+		{
+			CycleCount = 5;
+		}
+		else if (InstructionAddressingMode == AddressingMode::ZeroPageX || InstructionAddressingMode == AddressingMode::Absolute)
+		{
+			CycleCount = 6;
+		}
+		else if (InstructionAddressingMode == AddressingMode::AbsoluteX)
+		{
+			CycleCount = 7;
+		}
+		else
+		{
+			std::cerr << "LSR - Unknown addressing mode.\n";
+		}
+	}
 }
