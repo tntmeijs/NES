79161ee09b43130ea9c0e6842cbe4dcacb66467c
Tahar <tntmeijs@gmail.com>
2020-10-24 16:32:23 +0200

    DEC implementation

--- cpu/instructions/cpu_instruction_op_dec.cpp
+++ cpu/instructions/cpu_instruction_op_dec.cpp
@@ -1,10 +1,33 @@
 #include "cpu_instruction_op_dec.hpp"
 #include "cpu/cpu.hpp"
 
+#include <iostream>
+
 nes::CpuInstructionOpDEC::CpuInstructionOpDEC(CPU& cpuRef, AddressingMode addressingMode) :
 	CpuInstructionBase(cpuRef, addressingMode, "DEC")
 {}
 
 void nes::CpuInstructionOpDEC::ExecuteImpl()
 {
+	std::uint16_t targetAddress = CpuRef.GetTargetAddress(InstructionAddressingMode);
+	std::uint8_t value = CpuRef.ReadRamValueAtAddress(targetAddress);
+	--value;
+	CpuRef.WriteRamValueAtAddress(targetAddress, value);
+
+	if (InstructionAddressingMode == AddressingMode::ZeroPage)
+	{
+		CycleCount = 5;
+	}
+	else if (InstructionAddressingMode == AddressingMode::ZeroPageX || InstructionAddressingMode == AddressingMode::Absolute)
+	{
+		CycleCount = 6;
+	}
+	else if (InstructionAddressingMode == AddressingMode::AbsoluteX)
+	{
+		CycleCount = 7;
+	}
+	else
+	{
+		std::cerr << "DEC - Unknown addressing mode.\n";
+	}
 }
