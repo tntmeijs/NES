696a13dd249f91153e9172d0ec2428bb488328d7
Tahar <tntmeijs@gmail.com>
2020-12-26 15:33:33 +0100

    Removed debug clear color

--- editor/editor_game_window.cpp
+++ editor/editor_game_window.cpp
@@ -11,26 +11,5 @@ nes::EditorGameWindow::EditorGameWindow(
 
 void nes::EditorGameWindow::OnUpdate()
 {
-	static float r = 0, g = 0, b = 0;
-	r += 0.05f;
-	g += 0.10;
-	b += 0.15;
-
-	if (r >= 256)
-	{
-		r = 0;
-	}
-
-	if (g >= 256)
-	{
-		g = 0;
-	}
-
-	if (b >= 256)
-	{
-		b = 0;
-	}
-
-	//#DEBUG: change clear color into something sensible like white or black
-	clear(sf::Color(r, g, b));
+	clear();
 }
